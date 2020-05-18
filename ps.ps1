ff82c75862d975df6baff3a9bdf36219808a8260
ya-kay <ya-kay@gmx.de>
2020-05-18 12:03:22 +0200

    Delete ps.ps1

--- ps.ps1
+++
@@ -1,4 +0,0 @@
-$currentDateTime = Get-Date -Format "dd/MM/yyyy HH:mm K"
-$output = "C:\Users\yanik.kaypinger\Desktop\test2.txt"
-New-Item $output -type "file" | Out-Null 
-"This Document was machine-made on " + $currentDateTime | Out-File $output -Append
